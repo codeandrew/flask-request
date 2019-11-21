c709a3f325b97429ae7a293b468cc60d70d69463
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-11-21 13:50:11 +0800

    Update app, added url and port param

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
 from flask import Flask
-from flask_restful import Resource, Api
-import requests as req
+from flask_restful import Resource, Api, reqparse
+import requests
 
 app = Flask(__name__)
 api = Api(app)
 
-class Product(Resource):
+class Ping(Resource):
     def get(self):
-        url='http://example.org'
-        
-        res = req.get(url)
+        parser = reqparse.RequestParser()
+        parser.add_argument('url', type=str)
+        parser.add_argument('port', type=str)
 
-        print(res.text)
+        url=parser.parse_args().url
+        port=parser.parse_args().port
+
+        resp = requests.get(url + ":" + port)
+
+        print(resp.text)
         return [
-            str(res),
-            str(res.headers),
-            res.text
+            str(resp),
+            str(resp.headers),
+            resp.text,
         ]
 
-api.add_resource(Product, '/')
+api.add_resource(Ping, '/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0' , port=80, debug=True)
